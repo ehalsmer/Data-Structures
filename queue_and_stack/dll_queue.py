ee4ebfb132e3255b60575ea6edc9bafb614036d1
Eleasah Halsmer <eleasahjh@gmail.com>
2019-12-16 14:57:51 -0700

    dll_queue.py complete and passing tests

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -7,19 +7,19 @@ from doubly_linked_list import ListNode
 
 class Queue:
     def __init__(self):
-        # Why is our DLL a good choice to store our elements? Python lists are forbidden (we need not and may not use them)
+        self.size = 0
         self.storage = DoublyLinkedList()
-        self.size = self.storage.length
 
     def enqueue(self, value):
-        # should add an item to the back of the queue.
-        pass
+        self.size += 1
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
-        # should remove and return an item from the front of the queue.
-        pass
+        if self.size == 0:
+            return
+        else :
+            self.size -= 1
+            return self.storage.remove_from_head()
 
     def len(self):
-        # returns the number of items in the queue.
         return self.storage.length
-        # pass
