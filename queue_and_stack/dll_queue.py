acc32bdcc544341df9054d070836176625058c1d
Eleasah Halsmer <eleasahjh@gmail.com>
2019-12-16 14:22:24 -0700

    wip dll_stack.py

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,19 +1,25 @@
+# Check FAQ if import doesn't work
 import sys
-sys.path.append('../doubly_linked_list')
+sys.path.append('./doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
+from doubly_linked_list import ListNode
 
 
 class Queue:
     def __init__(self):
         self.size = 0
-        # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # Why is our DLL a good choice to store our elements? Python lists are forbidden (we need not and may not use them)
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
+        # should add an item to the back of the queue.
         pass
 
     def dequeue(self):
+        # should remove and return an item from the front of the queue.
         pass
 
     def len(self):
-        pass
+        # returns the number of items in the queue.
+        return self.storage.length
+        # pass
