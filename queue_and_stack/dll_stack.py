3ac068f14db65c5ea93c1ff0d9944afab571a124
Eleasah Halsmer <eleasahjh@gmail.com>
2019-12-16 14:40:13 -0700

    removed extra print statements

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -5,20 +5,14 @@ from doubly_linked_list import DoublyLinkedList
 class Stack:
     def __init__(self):
         self.size = 0
-        # Why is our DLL a good choice to store our elements?
         self.storage = DoublyLinkedList()
 
     def push(self, value):
         self.size += 1
-        print("Value we're pushing:", value)
         self.storage.add_to_head(value)
-        # print("STORAGE", self.storage)
 
     def pop(self):
-        # toReturn = self.storage.tail.value
-        print('In POP ')
         if self.size == 0:
-            print("self.size is 0")
             return
         else :
             self.size -= 1
